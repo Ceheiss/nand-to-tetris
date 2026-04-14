77fcb54c5efbc902c1579474db4c149f454d9c02
Ceheiss <cristobal.heiss.j@gmail.com>
2026-04-14 22:16:41 +0200

    parse simple assembly program

--- assembler/index.js
+++ assembler/index.js
@@ -26,6 +26,59 @@ const symbolTable = {
   'THAT': 4,
 };
 
+const compTable = {
+  '0': '0101010',
+  '1': '0111111',
+  '-1': '0111010',
+  'D': '0001100',
+  'A': '0110000',
+  '!D': '0001101',
+  '!A': '0110001',
+  '-D': '0001111',
+  '-A': '0110011',
+  'D+1': '0011111',
+  'A+1': '0110111',
+  'D-1': '0001110',
+  'A-1': '0110010',
+  'D+A': '0000010',
+  'D-A': '0010011',
+  'A-D': '0000111',
+  'D&A': '0000000',
+  'D|A': '0010101',
+  'M': '1110000',
+  '!M': '1110001',
+  '-M': '1110011',
+  'M+1': '1110111',
+  'M-1': '1110010',
+  'D+M': '1000010',
+  'D-M': '1010011',
+  'M-D': '1000111',
+  'D&M': '1000000',
+  'D|M': '1010101',
+}
+
+const destTable = {
+  '': '000',
+  'M': '001',
+  'D': '010',
+  'MD': '011',
+  'A': '100',
+  'AM': '101',
+  'AD': '110',
+  'AMD': '111',
+}
+
+const jumpTable = {
+  '': '000',
+  'JGT': '001',
+  'JEQ': '010',
+  'JGE': '011',
+  'JLT': '100',
+  'JNE': '101',
+  'JLE': '110',
+  'JMP': '111',
+}
+
 // pad the value with zeros to make it 16 bits
 const addFullBits = (value) => {
   const extraZeros = 16 - value.length;
@@ -62,6 +115,25 @@ async function parser() {
       }
     });
     console.log("aInstructions", aInstructions);
+
+    // handle C instructions
+    const cInstructions = aInstructions.map(line => {
+      if (/^.+=.+$/.test(line)) {
+        // get dest and comp values
+        const [dest, comp] = line.split('=');
+        // get comp and jump values
+        let [compValue, jumpValue] = comp.split(';');
+        // if jump value is not present, set it to empty string
+        jumpValue = jumpValue ? jumpValue : '';
+        // return the c instruction
+        return `111${compTable[compValue]}${destTable[dest]}${jumpTable[jumpValue]}`;
+      } else {
+        return line;
+      }
+    });
+    console.log("cInstructions", cInstructions);
+    // write the instructions to a file
+    await fs.writeFile('./test-files/Add.hack', cInstructions.join('\n'));
   } catch (err) {
     console.error(err);
   }
